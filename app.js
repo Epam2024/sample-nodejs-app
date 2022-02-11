2e3f7dee9e4cb9eeb8afa45d53b8563425af45a1
Ragu Thangavel <raguyazhin@gmail.com>
2022-02-11 10:29:15 +0530

    changed app.js content

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {res.send("<h1> Hello World </h1>");});
+app.get("/", (req, res) => {res.send("<h1> Hello World from AWS Code Pipeline </h1>");});
 
 app.get("/health", (req, res) => {
     res.status(200);
